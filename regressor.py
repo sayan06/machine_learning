e2232529c2b4be4398d988e8e310ef58bf2cc1cb
arindam das <arindam.gtalk@gmail.com>
2021-01-15 12:04:48 +0530

    Add space before and after function parameter

--- regressor.py
+++ regressor.py
@@ -4,7 +4,7 @@ from numpy import genfromtxt
 import matplotlib.pyplot as plt
 import numpy as np
 
-distanceData = genfromtxt('conversion.csv', delimiter=',')
+distanceData = genfromtxt('conversion.csv', delimiter = ',')
 # print(distanceData)
 inputX = distanceData[:,0]
 inputy = distanceData[:,1]
@@ -22,7 +22,7 @@ def hypothesis(X, weights):
 def getCost(weights, X, y):
     return (.5/len(X)) * np.sum(np.square(hypothesis(X,weights) - np.array(y)))
 
-def getGradient(weights,X,y):
+def getGradient(weights, X, y):
     grad_arr = [0, 0] 
     grad_arr[0] = (1/len(X)) * np.sum(hypothesis(X,weights) - np.array(y))
     grad_arr[1] = (1/len(X)) * np.sum((hypothesis(X,weights) - np.array(y)) * np.array(X))
@@ -54,9 +54,9 @@ def plotRegression(x):
     return minimizedWeights[0]+minimizedWeights[1]*x
 
 regressionLine = np.array(range(-5,60)) 
-plt.scatter(inputX,inputy, c = "red",alpha=.9, marker = 'o')
+plt.scatter(inputX,inputy, c = "red", alpha=.9, marker = 'o')
 plt.axis([0, 100, 0, 100])
-plt.plot(regressionLine,plotRegression(regressionLine))
+plt.plot(regressionLine, plotRegression(regressionLine))
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.show()
