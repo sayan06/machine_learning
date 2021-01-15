75a543a5348330ff1c499f99a28fffa56f04f740
arindam das <arindam.gtalk@gmail.com>
2021-01-15 12:27:30 +0530

    Refactoring

--- regressor.py
+++ regressor.py
@@ -1,60 +1,69 @@
 import csv
 import math
+import numpy as numpy
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
-import numpy as np
 
-distanceData = genfromtxt('conversion.csv', delimiter = ',')
-# print(distanceData)
-inputX = distanceData[:,0]
-inputy = distanceData[:,1]
+distanceData = genfromtxt('conversion.csv', delimiter=',')
 
+inputX = distanceData[:, 0]
+inputy = distanceData[:, 1]
 
-# plt.scatter(X,y, c = "red",alpha=.5, marker = 'o')
-# plt.xlabel('X')
-# plt.ylabel('Y')
-# plt.show()
 
 def hypothesis(X, weights):
-    return (weights[1] * np.array(X) + weights[0])
+    return (weights[1] * numpy.array(X) + weights[0])
 
 
 def getCost(weights, X, y):
-    return (.5/len(X)) * np.sum(np.square(hypothesis(X,weights) - np.array(y)))
+    return (.5/len(X)) * numpy.sum(numpy.square(hypothesis(X, weights) - numpy.array(y)))
+
 
 def getGradient(weights, X, y):
-    grad_arr = [0, 0] 
-    grad_arr[0] = (1/len(X)) * np.sum(hypothesis(X,weights) - np.array(y))
-    grad_arr[1] = (1/len(X)) * np.sum((hypothesis(X,weights) - np.array(y)) * np.array(X))
-    return grad_arr
+    gradientArray = [0, 0]
+    gradientArray[0] = (1/len(X)) * numpy.sum(hypothesis(X, weights) - numpy.array(y))
+    gradientArray[1] = (1/len(X)) * \
+        numpy.sum((hypothesis(X, weights) - numpy.array(y)) * numpy.array(X))
+    return gradientArray
+
 
 def minimize(weightsNew, weightsPrev, learningRate):
+
     print(weightsPrev)
-    print(getCost(weightsPrev,inputX,inputy))
-    iterations=0
+    print(getCost(weightsPrev, inputX, inputy))
+    
+    iterations = 0
+    
     while True:
         weightsPrev = weightsNew
-        w0 = weightsPrev[0] - learningRate * getGradient(weightsPrev,inputX,inputy)[0]
-        w1 = weightsPrev[1] - learningRate * getGradient(weightsPrev,inputX,inputy)[1]
+        w0 = weightsPrev[0] - learningRate * \
+            getGradient(weightsPrev, inputX, inputy)[0]
+        w1 = weightsPrev[1] - learningRate * \
+            getGradient(weightsPrev, inputX, inputy)[1]
         weightsNew = [w0, w1]
+    
         print(weightsNew)
-        print(getCost(weightsNew,inputX,inputy))
-        if (weightsNew[0]-weightsPrev[0])**2 + (weightsNew[1]-weightsPrev[1])**2 <= pow(10,-6):
+        print(getCost(weightsNew, inputX, inputy))
+    
+        if (weightsNew[0] - weightsPrev[0])** 2 + (weightsNew[1] - weightsPrev[1])** 2 <= pow(10, -6):
+            print('reached a low weight diff')
             return weightsNew
-        if iterations>500: 
+        if iterations > 500:
+            print('reached a 500 iterations')
             return weightsNew
-        iterations+= 1
+        iterations += 1
+
 
 weights = [0, -1]
-weightsMinimized = minimize(weights, weights, .1)
+weightsMinimized = minimize(weights, weights, .001)
 print(minimizedWeights)
 
-    
+
 def plotRegression(x):
     return minimizedWeights[0]+minimizedWeights[1]*x
 
-regressionLine = np.array(range(-5,60)) 
-plt.scatter(inputX,inputy, c = "red", alpha=.9, marker = 'o')
+
+regressionLine = numpy.array(range(-5, 60))
+plt.scatter(inputX, inputy, c="red", alpha=.9, marker='o')
 plt.axis([0, 100, 0, 100])
 plt.plot(regressionLine, plotRegression(regressionLine))
 plt.xlabel('X')
